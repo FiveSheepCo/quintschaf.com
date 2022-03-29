40e5ab76527e0620e6ab71d05e0338445ba5ac13
SplittyDev <splittydev@gmail.com>
2022-03-30 00:38:09 +0200

    Refactor AppSymbol

--- src/components/sfc/AppSymbol.js
+++ src/components/sfc/AppSymbol.js
@@ -1,23 +1,36 @@
 import style from '../scss/AppSymbol.scss'
 import { Link } from 'preact-router/match'
 
-const AppSymbol = ({ name, img, href, showShadow, showName, showBetaBadge }) => (
+const AppSymbol = ({ name, img, href, showShadow = false, showName = false, showBetaBadge = false, size = 128, showBorder = true, borderRadius = "2rem" }) => (
     <div class={style.symbol}>
         <Link href={href} class={style.linkWrapper}>
             <div
                 class={[style.iconWrapper, showShadow ? style.shadow : '']
                 .filter(c => c != '')
                 .join(' ')}
+                style={{
+                    width: `${size}px`,
+                    height: `${size}px`,
+                    maxWidth: `${size}px`,
+                    maxHeight: `${size}px`,
+                    borderRadius,
+                    border: showBorder ? 'solid .2rem white' : 'none',
+                }}
             >
                 <img
                     alt={name}
                     src={img}
-                    width={32}
-                    height={32}
+                    width={size}
+                    height={size}
                     class={style.icon}
+                    style={{
+                        transform: `scale(0.25)`,
+                        width: `${size * 4}px`,
+                        height: `${size * 4}px`
+                    }}
                 />
             </div>
-            { showBetaBadge && <span class={style.betaBadge}>Beta</span> }
+            {showBetaBadge && <span class={style.betaBadge}>Beta</span>}
             {showName && <span class={style.appName}>{name}</span>}
         </Link>
     </div>
